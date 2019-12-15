988fedf6509ca64a2bed3bdf6172c12866c7a134
PengshuoLiu97 <pliu285@uwo.ca>
2019-12-15 17:42:07 -0500

    Login UI

--- src/app/share/wy-ui/wy-layer/wy-layer.module.ts
+++ src/app/share/wy-ui/wy-layer/wy-layer.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WyLayerModalComponent } from './wy-layer-modal/wy-layer-modal.component';
+import { WyLayerDefaultComponent } from './wy-layer-default/wy-layer-default.component';
 
 
 
 @NgModule({
-  declarations: [WyLayerModalComponent],
+  declarations: [WyLayerModalComponent, WyLayerDefaultComponent],
   imports: [
     CommonModule
   ],
-  exports: [WyLayerModalComponent]
+  exports: [WyLayerModalComponent, WyLayerDefaultComponent]
 })
 export class WyLayerModule { }
