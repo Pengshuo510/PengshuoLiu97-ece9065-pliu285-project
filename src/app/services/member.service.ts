affafbbcc1e025ede1093787f3ae44f704831dfd
PengshuoLiu97 <pliu285@uwo.ca>
2019-12-15 21:57:09 -0500

    Modify 2

---
+++ src/app/services/member.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Inject } from '@angular/core';
+import { ServicesModule, API_CONFIG } from './services.module';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SampleBack } from './data-types/common.types';
+import { map } from 'rxjs/internal/operators';
+import { LoginParams } from '../share/wy-ui/wy-layer/wy-layer-login/wy-layer-login.component';
+import { User } from './data-types/member.type';
+import queryString from 'query-string';
+
+@Injectable({
+  providedIn: ServicesModule
+})
+export class MemberService {
+  constructor(private http: HttpClient, @Inject(API_CONFIG) private uri: string) { }
+  login(formValue: LoginParams): Observable<User> {
+    const params = new HttpParams({ fromString: queryString.stringify(formValue) });
+    return this.http.get(this.uri + 'login/cellphone', { params })
+    .pipe(map(res => res as User));
+  }
+
+
+  // 获取用户详情
+  getUserDetail(uid: string): Observable<User> {
+    const params = new HttpParams({ fromString: queryString.stringify({ uid }) });
+    return this.http.get(this.uri + 'user/detail', { params })
+    .pipe(map(res => res as User));
+  }
+
+  // 退出
+  logout(): Observable<SampleBack> {
+    return this.http.get(this.uri + 'logout')
+    .pipe(map(res => res as SampleBack));
+  }
+}
