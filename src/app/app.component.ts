48fa14865e39044c3e1760def3462a541400db53
PengshuoLiu97 <pliu285@uwo.ca>
2019-12-15 21:54:25 -0500

    Reviews and marks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 import { SearchService } from './services/search.service';
 import { SearchResult } from './services/data-types/common.types';
 import { isEmptyObject } from './utils/tools';
+import { ModalTypes } from './store/reducers/member.reducer';
+import { AppStoreModule } from './store/index';
+import { Store } from '@ngrx/store';
+import { SetModalType, SetUserId } from './store/actions/member.actions';
+import { BatchActionsService } from './store/batch-actions.service';
+import { LoginParams } from './share/wy-ui/wy-layer/wy-layer-login/wy-layer-login.component';
+import { MemberService } from './services/member.service';
+import { User } from './services/data-types/member.type';
+import { NzMessageService } from 'ng-zorro-antd';
+import { codeJson } from './utils/base64';
+import { StorageService } from './services/storage.service';
 
 @Component({
   selector: 'app-root',
@@ -18,12 +29,41 @@ export class AppComponent {
   }];
 
   searchResult: SearchResult;
-
+  wyRememberLogin: LoginParams;
+  user: User;
   constructor(
-    private searchServe: SearchService
+    private searchServe: SearchService,
+    private store$: Store<AppStoreModule>,
+    private batchActionsServe: BatchActionsService,
+    private memberServe: MemberService,
+    private messageServe: NzMessageService,
+    private storageServe: StorageService,
   ) {
+    const userId = this.storageServe.getStorage('wyUserId');
+    if (userId) {
+      this.store$.dispatch(SetUserId({ id: userId }));
+      this.memberServe.getUserDetail(userId).subscribe(user => this.user = user);
+    }
 
+    const wyRememberLogin = this.storageServe.getStorage('wyRememberLogin');
+    if (wyRememberLogin) {
+      this.wyRememberLogin = JSON.parse(wyRememberLogin);
+    }
   }
+
+
+  // 打开弹窗
+  openModal(type: ModalTypes) {
+    this.batchActionsServe.controlModal(true, type);
+  }
+
+
+  // 改变弹窗类型
+  onChangeModalType(modalType = ModalTypes.Default) {
+    this.store$.dispatch(SetModalType({ modalType }));
+  }
+
+
   onSearch(keywords: string) {
     console.log('keywords :', keywords);
     if (keywords) {
@@ -31,7 +71,7 @@ export class AppComponent {
         this.searchResult = this.highlightKeyWords(keywords, res);
         console.log('searchResult :', this.searchResult);
       });
-    }else {
+    } else {
       this.searchResult = {};
     }
   }
@@ -48,5 +88,47 @@ export class AppComponent {
       });
     }
     return result;
+  }
+
+
+  // 登陆
+  onLogin(params: LoginParams) {
+    this.memberServe.login(params).subscribe(user => {
+      this.user = user;
+      this.batchActionsServe.controlModal(false);
+      this.alertMessage('success', '登陆成功');
+      this.storageServe.setStorage({
+        key: 'wyUserId',
+        value: user.profile.userId
+      });
+      this.store$.dispatch(SetUserId({ id: user.profile.userId.toString() }));
+
+      if (params.remember) {
+        this.storageServe.setStorage({
+          key: 'wyRememberLogin',
+          value: JSON.stringify(codeJson(params))
+        });
+      } else {
+        this.storageServe.removeStorage('wyRememberLogin');
+      }
+    }, ({ error }) => {
+      // console.log('error :', error);
+      this.alertMessage('error', error.message || '登陆失败');
+    });
+  }
+
+  onLogout() {
+    this.memberServe.logout().subscribe(() => {
+      this.user = null;
+      this.storageServe.removeStorage('wyUserId');
+      this.store$.dispatch(SetUserId({ id: '' }));
+      this.alertMessage('success', '已退出');
+    }, ({ error }) => {
+      this.alertMessage('error', error.message || '退出失败');
+    });
+  }
+
+  private alertMessage(type: string, msg: string) {
+    this.messageServe.create(type, msg);
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,13 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { map } from 'rxjs/internal/operators';
 import { SheetService } from 'src/app/services/sheet.service';
 import { BatchActionsService } from '../../store/batch-actions.service';
+import { ModalTypes } from '../../store/reducers/member.reducer';
+import { User } from 'src/app/services/data-types/member.type';
+import { AppStoreModule } from '../../store/index';
+import { Store, select } from '@ngrx/store';
+import { getUserId } from '../../store/selectors/member.selector';
+import { MemberService } from 'src/app/services/member.service';
+import { SetUserId } from 'src/app/store/actions/member.actions';
 
 @Component({
   selector: 'app-home',
@@ -17,6 +24,7 @@ export class HomeComponent implements OnInit {
   hotTags: HotTag[];
   songSheetList: SongSheet[];
   singers: Singer[];
+  user: User;
 
   @ViewChild(NzCarouselComponent, { static: true }) private nzCarousel: NzCarouselComponent;
 
@@ -24,7 +32,9 @@ export class HomeComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private sheetServe: SheetService,
-    private batchActionsServe: BatchActionsService
+    private batchActionsServe: BatchActionsService,
+    private store$: Store<AppStoreModule>,
+    private memberServe: MemberService,
   ) {
     this.route.data.pipe(map(res => res.homeDatas)).subscribe(([banners, hotTags, songSheetList, singers]) => {
       this.banners = banners;
@@ -32,10 +42,21 @@ export class HomeComponent implements OnInit {
       this.songSheetList = songSheetList;
       this.singers = singers;
     });
-    
+    this.store$.pipe(select('member'), select(getUserId)).subscribe(id => {
+      console.log('id :', id);
+      if (id) {
+        this.getUserDetail(id);
+      } else {
+        this.user = null;
+      }
+    });
   }
 
   ngOnInit() {
+  }
+
+  private getUserDetail(id: string) {
+    this.memberServe.getUserDetail(id).subscribe(user => this.user = user);
   }
 
   onBeforeChange({ to }) {
@@ -58,6 +79,6 @@ export class HomeComponent implements OnInit {
   }
 
   openModal() {
-    this.batchActionsServe.controlModal();
+    this.batchActionsServe.controlModal(true, ModalTypes.Default);
   }
 }
